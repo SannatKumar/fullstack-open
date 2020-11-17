f339fb62237aff0638ffa5daab2fe5d5c20ae646
Raj Kumar <interplaytheheads@gmail.com>
2020-11-17 13:23:36 +0200

    Footer created and defined in the app component

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ const App = () => {
       <Hello name={name} age= {age} />
       <Hello name="Raj Kumar" age = "25" />
       <Hello name="Krishna" age = {25+6}/>
+      <Footer />
+    </div>
+  )
+}
+const Footer = () => {
+  return(
+    <div>
+      Greeting App created by <a href = "https://github.com/SannatKumar">Raj Kumar</a>
     </div>
   )
 }
