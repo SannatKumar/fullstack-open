8dfae993fd0624384fa7c063dbef45fc17348878
Raj Kumar <interplaytheheads@gmail.com>
2020-11-17 13:27:01 +0200

    div removed and fragmented

--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
   return(
-    <div>
+    <>
       <p>{props.name}, You are {props.age} years old.</p>
-    </div>
+    </>
   )
 }
 const App = () => {
   const name= 'Maya';
   const age= 32;
   return(
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name={name} age= {age} />
       <Hello name="Raj Kumar" age = "25" />
       <Hello name="Krishna" age = {25+6}/>
       <Footer />
-    </div>
+    </>  
   )
 }
 const Footer = () => {
   return(
-    <div>
+    <>
       Greeting App created by <a href = "https://github.com/SannatKumar">Raj Kumar</a>
-    </div>
+    </>
   )
 }
 ReactDOM.render(<App />,  document.getElementById('root'));
